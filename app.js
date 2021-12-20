4351996bfc235b29e86b3ab4195fa2ba4cf34a0a
mike <mike@mikedeiMac.local>
2021-12-20 14:53:57 +0800

    feature: menu filter js

---
+++ app.js
@@ -0,0 +1,132 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenu(menu);
+  displauBnt();
+});
+
+const displayMenu = (itemMenu) => {
+  let tempMenu = itemMenu
+    .map((item) => {
+      return `<article class="menu">
+    <img src=${item.img} alt="" class="img" />
+    <header>
+      <h5>${item.title}</h5>
+      <p>${item.price}</p>
+    </header>
+    <p>
+      ${item.desc}
+    </p>
+  </article>`;
+    })
+    .join("");
+  sectionCenter.innerHTML = tempMenu;
+};
+
+const displauBnt = () => {
+  const values = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const btns = values
+    .map((value) => {
+      return `<button class="btn" data-id=${value}>${value}</button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = btns;
+
+  const menuBtns = btnContainer.querySelectorAll(".btn");
+  menuBtns.forEach(function (menuBtn) {
+    menuBtn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const categoryMenu = menu.filter((item) => item.category === category);
+      console.log(categoryMenu);
+      if (category === "all") {
+        return displayMenu(menu);
+      }
+      displayMenu(categoryMenu);
+    });
+  });
+};
